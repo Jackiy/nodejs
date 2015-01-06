716ad10cc9c414ac41ce94ca398f75061ec98de0
jackiy <jackiy.huang@hotmail.com>
2015-01-06 13:09:02 +0000

    Add the mongodb'

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var jobModel = require("./models/Job");
 
 var app = express();
 
@@ -7,9 +9,25 @@ app.set("view engine", "jade");
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/api/jobs", function(req, res){
+    mongoose.model('Job').find({}).exec(function(error, collection){
+        res.send(collection);
+    })
+});
 
 app.get("*", function(req, res){
     res.render("index");
+});
+
+//mongoose.connect("mongodb://localhost/jobfinder");
+
+mongoose.connect("mongodb://psdev:psdev@ds031601.mongolab.com:31601/jobfinder");
+
+var con = mongoose.connection;
+
+con.once('open', function(){
+    console.log('connected to mongodb successfully.');
+    jobModel.seedJobs();
 });
 
 app.listen(process.env.PORT, process.env.IP);
